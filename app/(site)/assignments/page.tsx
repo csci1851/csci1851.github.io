d42a221107e100e38cbd50cac1cf0fd753d22970
falakpabari <113410959+falakpabari@users.noreply.github.com>
2026-03-07 20:59:39 -0500

    minor change

--- app/(site)/assignments/page.tsx
+++ app/(site)/assignments/page.tsx
@@ -165,7 +165,7 @@ export default function AssignmentsPage() {
               If you have questions about assignments or need clarification on requirements:
             </p>
             <ul className="list-disc list-inside space-y-2 ml-4">
-              <li>Attend office hours (Fridays, 3:30 PM - 5:30 PM, DSI 313 or Zoom, HTA office hours: 1:00 PM - 3:00 PM, CIT 207)</li>
+              <li>Attend office hours (Fridays, 3:30 PM - 5:30 PM, DSI 313 or Zoom, HTA office hours: 1:00 PM - 3:00 PM, CIT 367)</li>
               <li>Post questions on the course discussion forum</li>
               <li>Email the teaching staff for private concerns</li>
             </ul>
